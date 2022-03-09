43f148383da01efd0acebb92efe0798ee6385e59
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-03-09 09:42:59 +0100

    fix upload route second try

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ router.get("/", (req, res, next) => {
   res.json("Welcome to Amigo's API");
 });
 
-router.get("/upload", (req, res, next) => {
-  res.json("upload files here");
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  res.json({ imageUrl: req.file.path });
 });
 
 module.exports = router;
